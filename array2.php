c4c4d033b844aadcd587a808e069d01064db6205
Daniil Yanishevsky <danilyanich65@gmail.com>
2020-12-24 15:34:55 +0500

    Домашнее задание за 23.12.2020

---
+++ array2.php
@@ -0,0 +1,61 @@
+<?php
+echo "Задание 1.<br>";
+$arr1 = array();
+$n = 10; $i = 0;
+while($i < $n)
+{
+    $arr1[$i] = rand(0, 50);
+    if ($i%2==0)
+    {
+        echo "<strong>$arr1[$i]</strong><br>";
+    }
+    elseif ($i%2!=0)
+    {
+        echo "$arr1[$i]<br>";
+    }
+    $i++;
+}
+echo "<br>Задание 2.";
+$arr2 = array(
+              'fruits' => array('banana', 'apple', 'mango', 'avocado'),
+              'gods' => array('andromeda', 'aphrodite', 'ares', 'zeus'),
+              'professions' => array('artist', 'chef', 'astrologer', 'doctor')
+             );
+foreach ($arr2 as $key => $item)
+{
+    echo "<br>$key:<br>";
+    if (is_array($item))
+        foreach ($item as $item2)
+        {
+            if ($item2[0] == 'a')
+                echo '  ' .$item2 . ' ';
+        }
+    elseif ($item[0] == 'a')
+        echo $item[0].' ';
+}
+echo '<br>';
+echo "<br>Задание 3.<br>";
+$counter1 = 0;
+$counter2 = 0;
+$counter3 = 0;
+$arr3 = array(
+    '0' => array('23', '54', '31', '42', '83', '9'),
+    '1' => array('15', '63', '28', '70', '55'),
+    '2' => array('37', '96', '67', '39')
+);
+foreach ($arr3 as $key1 => $item3)
+{
+    echo "<br>$key1: ";
+    if (is_array($item3))
+        foreach ($item3 as $item4)
+        {
+            $counter2 += 1;
+            $counter3 += 1;
+        }
+        echo $counter2;
+        $counter2 = 0;
+    $counter1 += 1;
+}
+$counter3 += $counter1;
+echo '<br>'.'Общее количесвто элементов  массива: '.$counter3;
+
