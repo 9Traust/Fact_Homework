a8bf1b68111f2de97c83306846939f4536af1051
Daniil Yanishevsky <danilyanich65@gmail.com>
2021-01-13 18:36:38 +0500

    Домашнее задание за 11.01.21

---
+++ phpcourse.php
@@ -0,0 +1,24 @@
+<?php
+
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<form action="phpcourse.php" method="get">
+    <select name="select">
+        <option value="l1">Лаб. 1</option>
+        <option value="l2">Лаб. 2</option>
+        <option value="l3">Лаб. 3</option>
+        <option value="l4">Лаб. 4</option>
+    </select>
+    <button type="submit">Confirm</button>
+</form>
+</body>
+</html>
